cfdc58bd5f5bd055b07a30cdb049386060583f47
gabastil <glenn.abastillas@gmail.com>
2020-05-01 17:23:59 -0400

    Updated validate.py

--- sms/py/validate.py
+++ sms/py/validate.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # author: Glenn Abastillas
 # created: May 1, 2020
-# description: A simple script to verify phone numbers
+# description: A simple script to validate phone numbers
 
 from sys import argv
 import json
@@ -9,23 +9,18 @@ from tqdm import tqdm
 import pandas as pd
 import requests
 
-endpoint = "http://apilayer.net/api/validate?"
+endpoint = "http://apilayer.net/api/validate?" +\
+           "access_key={}&number={}&country_code={}"
 
-def validate_number(key, number, country_code):
+
+def validate_number(access_key, number, country_code):
     ''' Return a tuple with information about a phone number '''
 
-    access_key = f"access_key={key}"
-    number = f"&number={int(number)}"
-    country_code = f"&country_code={country_code}"
-
-    api = f'{endpoint}{access_key}{number}{country_code}'
-
-    # print(api)
+    api = endpoint.format(access_key, int(number), country_code)
     result = requests.get(api)
 
     if result.status_code == 200:
         response = json.loads(result.content)
-        # print(response)
         if 'valid' not in response:
             return None
         return response['valid']
@@ -45,7 +40,7 @@ if __name__ == "__main__":
     data = pd.read_csv("../data/numbers.csv")
     total = (data.checked == False).sum()
 
-    is_valid, is_valid_count = [], 0
+    is_valid, valid_count = [], 0
 
     for i, row in tqdm(data.iterrows(), total=total):
 
@@ -53,7 +48,7 @@ if __name__ == "__main__":
             valid_ = validate_number(user_access_key, row.number, country_code)
 
             if valid_:
-                is_valid_count += 1
+                valid_count += 1
                 data.loc[i, 'is_valid'] = True
 
             if valid_ is None:
@@ -62,4 +57,4 @@ if __name__ == "__main__":
             data.loc[i, 'checked'] = True
 
     data.to_csv("../data/numbers_validated.csv", index=False)
-    print(f'{is_valid_count / data.shape[0] * 100:.1%} valid numbers found.')
+    print(f'{valid_count / total * 100:.1%} of {total} are valid.')
