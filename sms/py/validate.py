aefaa72f47ed91aebfae0f0f0bdfa3e467c0c813
gabastil <glenn.abastillas@gmail.com>
2020-05-01 21:21:12 -0400

    Updated validate.py and added readme.md

--- sms/py/validate.py
+++ sms/py/validate.py
@@ -9,8 +9,7 @@ import pandas as pd
 import requests
 import json
 
-url = ("http://apilayer.net/api/validate?"
-       "access_key={}&number={}&country_code={}")
+url = "http://apilayer.net/api/validate?access_key={}&number={}&country_code={}"
 
 
 def validate(access_key, number, country_code):
@@ -27,9 +26,8 @@ def validate(access_key, number, country_code):
             True or false if the API provides a proper response.
             None if the API does not provide a proper response.
     '''
-
-    api = url.format(access_key, int(number), country_code)
-    result = requests.get(api)
+    request = url.format(access_key, number, country_code)
+    result = requests.get(request)
 
     if result.status_code == 200:
         response = json.loads(result.content)
@@ -42,33 +40,36 @@ def validate(access_key, number, country_code):
 
 if __name__ == "__main__":
     user_access_key, country_code = argv[1:3]
+    data_path = "../data/numbers.csv"
 
     # Ensure user access key is of valid type and length
     assert isinstance(user_access_key, str) and len(user_access_key) < 33
 
-    print(f'\nPhone Number Verification'
-          f'\nUser Access Key: {user_access_key}'
-          f'\nCountry Code: {country_code}'
-          f'\n{"":->80}')
+    header = 'Phone Number Verification\nUser Access Key:{}\nCountry Code:{}\n'
+    print(header.format(user_access_key, country_code))
 
-    data = pd.read_csv("../data/numbers.csv")
+    data = pd.read_csv(data_path)
+
     total = (data.checked == False).sum()
-
-    valid_count = 0
+    count = 0
 
     for i, row in tqdm(data.iterrows(), total=total):
 
         if row.checked == False:
-            valid_ = validate(user_access_key, row.number, country_code)
+            result = validate(user_access_key, int(row.number), country_code)
 
-            if valid_ is None:
+            if result is None:
                 break
 
-            elif valid_:
-                valid_count += 1
+            elif result:
+                count += 1
                 data.loc[i, 'is_valid'] = True
+
+            else:
+                data.loc[i, 'is_valid'] = False
 
             data.loc[i, 'checked'] = True
 
-    data.to_csv("../data/numbers.csv", index=False)
-    print(f'{valid_count / total * 100:.1%} of {total} are valid.')
+    data.to_csv(data_path, index=False)
+    percent = count / total * 100
+    print(f'{percent:.1%} of {total} are valid.')
