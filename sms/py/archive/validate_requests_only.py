c323f5abdd9f581eafaf33e141e75b561e45ae52
gabastil <glenn.abastillas@gmail.com>
2020-05-03 11:14:08 -0400

    Removed dependencies from validate.py

--- sms/py/archive/validate_requests_only.py
+++ sms/py/archive/validate_requests_only.py
@@ -3,14 +3,14 @@
 # created: May 1, 2020
 # description: A simple script to validate phone numbers
 
+from sys import argv
 from urllib.request import Request, urlopen
 from urllib.error import HTTPError, URLError
-from urllib.parse import urlencode
-from sys import argv
+import requests
 import json
 import csv
 
-url = "http://apilayer.net/api/validate?"
+url = "http://apilayer.net/api/validate?access_key={}&number={}&country_code={}"
 
 
 def validate(access_key, number, country_code):
@@ -20,29 +20,21 @@ def validate(access_key, number, country_code):
         ----------
             access_key (str) : 32-digit user access key for API
             number (str, int) : Phone number to check
-            country_code (str) : 2-letter country code for the phone number
+            country_code (str) : 2 character country code for the phone number
 
         Returns
         -------
             True or false if the API provides a proper response.
             None if the API does not provide a proper response.
     '''
-    try:
-        query = urlencode({'access_key': access_key,
-                           'number': number,
-                           'country_code': country_code})
+    request = url.format(access_key, number, country_code)
+    result = requests.get(request)
 
-        result = urlopen(url + query)
+    if result.status_code == 200:
+        response = json.loads(result.content)
 
-        if result.status == 200:
-            response = json.loads(result.read())
-
-            if 'valid' in response:
-                return response['valid']
-            print(response['error']['info'])
-
-    except (HTTPError, URLError) as e:
-        print(e.reason)
+        if 'valid' in response:
+            return response['valid']
 
 
 def read_csv(path):
@@ -56,35 +48,41 @@ def read_csv(path):
 def to_csv(data, path):
     data = [','.join(line) for line in data]
     with open(path, 'w') as numbers_data:
-        numbers_data.write('\n'.join(data))
+        data = '\n'.join(data)
+        numbers_data.write(data)
 
 
 def checked(check):
-    ''' Indicate whether or not a phone number has already been checked '''
+    ''' Determine if the number has already been checked '''
     return check.lower().startswith('t')
 
 
 if __name__ == "__main__":
     user_access_key, country_code = argv[1:3]
     data_path = "../data/numbers.csv"
-    print(f'\nPhone Number Verification for {country_code}\n{"":-<80}')
+    print(f'Phone Number Verification with Key {user_access_key}\n{"":-<80}')
 
     # Ensure user access key is of valid type and length
     assert isinstance(user_access_key, str) and len(user_access_key) < 33
 
-    numbers = read_csv(data_path)
+    header, *numbers = read_csv(data_path)
 
     # Assumes indices : 'number' at 0, 'is_valid' at 1, 'checked' at 2
-    for row in numbers[1:]:
+    for row in numbers:
 
         if not checked(row[2]):
             result = validate(user_access_key, int(row[0]), country_code)
 
             if result is None:
+                print('Call limit likely reached.')
                 break
 
-            row[1] = 'True' if result else 'False'
+            row[1] = 'False'
+
+            if result:
+                row[1] = 'True'
+
             row[2] = 'True'
 
-    to_csv(numbers, data_path)
-    print(f'{"":-<80}\nVerification Process Completed')
+    to_csv([header] + numbers, data_path)
+    print(f'Verification loop completed.')
